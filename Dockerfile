b4cff12bcfda55052e8cc04ff03ecf0f3a49de22
Michael Kavanagh <michaeljkavanagh@gmail.com>
2022-04-13 16:07:12 +0100

    latest

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:16-alpine
-
-WORKDIR /usr/app
-
-COPY ./ ./
-RUN npm install
-
-CMD ["npm", "start"]
--- node_modules/@mapbox/mapbox-gl-draw/bench/lib/format_number.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-export default function formatNumber(x) {
-  return Math.round(x).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
